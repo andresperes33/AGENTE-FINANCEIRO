40eb5c8917bde52bac5639dfb15a21c387a337ed
andresperes33 <andresperesdev@gmail.com>
2026-02-17 14:28:24 -0300

    FIX CRITICO: Corrige URLs de password reset e template de email

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -14,15 +14,29 @@ urlpatterns = [
     
     # Reset de senha
     path('password-reset/', 
-         auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'),
+         auth_views.PasswordResetView.as_view(
+             template_name='accounts/password_reset.html',
+             email_template_name='accounts/password_reset_email.html',
+             success_url=reverse_lazy('accounts:password_reset_done')
+         ),
          name='password_reset'),
+         
     path('password-reset/done/',
-         auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='accounts/password_reset_done.html'
+         ),
          name='password_reset_done'),
+         
     path('password-reset-confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'),
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='accounts/password_reset_confirm.html',
+             success_url=reverse_lazy('accounts:password_reset_complete')
+         ),
          name='password_reset_confirm'),
+         
     path('password-reset-complete/',
-         auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='accounts/password_reset_complete.html'
+         ),
          name='password_reset_complete'),
 ]
